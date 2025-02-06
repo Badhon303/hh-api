40974fb617aaa07fa184b64a33d1be4a2226af54
BadhonDev <badhon@ebiw.com>
2025-02-06 17:51:57 +0600

    media read true

--- src/collections/ImageMedia/index.js
+++ src/collections/ImageMedia/index.js
@@ -5,24 +5,25 @@ import { MediaAdminAndUser } from '../../access/mediaAdminAndUser'
 export const ImageMedia = {
   slug: 'media-images',
   access: {
-    read: ({ req: { params, user } }) => {
-      if (user?.role === 'admin' || user?.role === 'super-admin') {
-        // Grant full access to all images for admins
-        return true
-      }
+    // read: ({ req: { params, user } }) => {
+    //   if (user?.role === 'admin' || user?.role === 'super-admin') {
+    //     // Grant full access to all images for admins
+    //     return true
+    //   }
 
-      if (params && params.id) {
-        // Grant public access for requests with a specific `id`
-        return {
-          id: {
-            equals: params.id,
-          },
-        }
-      }
+    //   if (params && params.id) {
+    //     // Grant public access for requests with a specific `id`
+    //     return {
+    //       id: {
+    //         equals: params.id,
+    //       },
+    //     }
+    //   }
 
-      // Deny access otherwise
-      return false
-    },
+    //   // Deny access otherwise
+    //   return false
+    // },
+    read: () => true,
     update: () => false,
     delete: () => MediaAdminAndUser,
   },
